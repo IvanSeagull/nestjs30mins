678de481eed0d075a64a8201c5240129c76b44b7
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2022-05-07 17:10:01 +0100

    Finished

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule} from '@nestjs/typeorm';
+import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+import { Post } from './posts/entities/post.entity';
+
+
+const config: SqliteConnectionOptions = {
+  type: 'sqlite',
+  database: '../db',
+  entities: [Post],
+  synchronize: true,
+}
+
+@Module({
+  imports: [PostsModule, TypeOrmModule.forRoot(config)],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
